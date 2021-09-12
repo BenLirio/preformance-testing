1a7d9155d7c38dd89df9bac8f09582c8213e5849
BenLirio <benjlirio@gmail.com>
2021-09-12 20:28:08 +0000

    times a for 1<<22 operations

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	start := time.Now()
+	for i := 0; i < 1<<22; i++ {
+
+	}
+	duration := time.Since(start)
+	fmt.Println(duration)
 }
